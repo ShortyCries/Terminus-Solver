1ff6ffc8d9da0d6d4dc7c372c8ebfd395357ce9a
hossam laila <hossamemad2011@hotmail.com>
2025-03-02 13:24:41 +0200

    fixing directory for imgaes

--- src/Signs.jsx
+++ src/Signs.jsx
@@ -4,7 +4,7 @@ export default function Signs({ value, clickFunc, isSelected, onShow }) {
 
     const obj = { '22': "img1", 21: "img2", 20: "img3", 11: "img4", 10: "img5", 0: "img6" }
 
-    const src = './public/' + obj[value] + '.png';
+    const src = './dist/assets/' + obj[value] + '.png';
 
 
 
